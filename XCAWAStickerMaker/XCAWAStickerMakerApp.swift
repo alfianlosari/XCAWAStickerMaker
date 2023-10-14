0b046471b73e79b0aadcc3654fc310629505f7cd
Alfian Losari <alfianlosari@gmail.com>
2023-10-14 21:31:28 +0700

    Initial Commit

---
+++ XCAWAStickerMaker/XCAWAStickerMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAWAStickerMakerApp.swift
+//  XCAWAStickerMaker
+//
+//  Created by Alfian Losari on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct XCAWAStickerMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
